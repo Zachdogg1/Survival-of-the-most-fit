4dad2b78ec8c5952ac602e7a746d5584f9a50aa3
Zach <zsgray2000@gmail.com>
2017-01-25 13:47:18 -0600

    prototype 1
    yes

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,32 +2,64 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.TimeUtils;
+
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	private Texture dirt;
+	private Texture tree;
+	private Texture stone;
+	private Music russiasong;
+	private SpriteBatch batch;
+	private OrthographicCamera camera;
+	private Rectangle dirtspace;
 	
 	@Override
 	public void create () {
+		dirt = new Texture("data/DarkDirt.jpg");
+		tree = new Texture(("data/BigTree1.png"));
+		stone = new Texture(("data/download.jpg"));
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 1920, 1080);
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		dirtspace = new Rectangle();
+		dirtspace.x = 800 / 2 - 64 / 2;
+		dirtspace.y = 20;
+		dirtspace.width = 64;
+		dirtspace.height = 64;
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		camera.update();
+		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(dirt, dirtspace.x, dirtspace.y);
 		batch.end();
+		if(Gdx.input.isTouched()) {
+			Vector3 touchPos = new Vector3();
+			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+			camera.unproject(touchPos);
+			
+		}
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+
 	}
 }
--- core/src/com/mygdx/game/importation.java
+++ core/src/com/mygdx/game/importation.java
@@ -8,44 +8,13 @@ import com.badlogic.gdx.graphics.Texture;
  * Created by 256233 on 1/23/2017.
  */
 public class importation extends ApplicationAdapter {
-    private Texture dirt;
-    private Texture tree;
-    private Texture stone;
-    private Music russiasong;
-    private OrthographicCamera camera;
-    private SpriteBatch groundtexture;
-    private Rectangle dirtspace;
+
     @Override
     public void create(){
-        dirt = new Texture(Gdx.files.internal("DarkDirt.jpg"));
-        tree = new Texture(Gdx.files.internal("BigTree1.png"));
-        stone = new Texture(Gdx.files.internal("download.jpg"));
-        camera = new OrthographicCamera();
-        camera.setToOrtho(false, 1920, 1080);
-        russiasong = Gdx.audio.newMusic(Gdx.files.internal("russiasong.mp3"));
-        russiasong.setLooping(true);
-        russiasong.play();
-        groundtexture = new SpriteBatch();
-        dirtspace = new Rectangle();
-        dirtspace.x = 800 / 2 - 64 / 2;
-        dirtspace.y = 20;
-        dirtspace.width = 64;
-        dirtspace.height = 64;
+
     }
     @Override
     public void render() {
-        Gdx.gl.glClearColor(0, 0, 0.2f, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        camera.update();
-        batch.setProjectionMatrix(camera.combined);
-        batch.begin();
-        batch.draw(dirt, dirtspace.x, dirtspace.y);
-        batch.end();
-        if(Gdx.input.isTouched()) {
-            Vector3 touchPos = new Vector3();
-            touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
-            camera.unproject(touchPos);
-            dirtspace.x = touchPos.x - 64 / 2;
-        }
+
     }
 }
